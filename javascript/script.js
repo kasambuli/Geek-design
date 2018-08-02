cfef38d84bc63e6ad5e9f3c455c28c3d9b920b12
kasambuli <cynthiakasambuli@gmail.com>
2018-08-02 18:16:47 +0300

    add javascript transitions

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+    $("#goToSignUp").click(function() {
+        console.log("jj");
+        $(".login").removeClass("fadeIn")
+        $(".login").addClass("fadeOut")
+        $(".login").hide()
+
+        $(".signup").show()
+        $(".signup").removeClass("fadeOut")
+        $(".signup").addClass("fadeIn")
+
+    })
+
+
+    $("#goToSignIn").click(function() {
+        $(".signup").removeClass("fadeIn")
+        $(".signup").addClass("fadeOut")
+
+        $(".signup").hide()
+        $(".login").show()
+
+        $(".login").removeClass("fadeOut")
+        $(".login").addClass("fadeIn")
+
+
+
+
+    })
+
+
+})
